bf7c82e5646e56754e1bb3ab1060f3b8eb756a6a
blumfontein <blumfontein@example.com>
2016-11-13 17:02:54 +0600

    Install normalize.css

--- src/config/services/shared.php
+++ src/config/services/shared.php
@@ -98,4 +98,17 @@ return [
             'lifetime' => 60 * 86400
         ]]
     ],
+
+    'twig' => [
+        'shared' => true,
+        'class' => 'Twig_Environment',
+        'arguments' => ['#twig.filesystem_loader', [
+            'cache' => '@dir/twig_cache',
+            'debug' => '@twig/debug'
+        ]],
+        'after' => function(\Perfumer\Component\Container\Container $container, \Twig_Environment $twig) {
+            $twig->addExtension($container->get('twig.framework_extension'));
+            $twig->addExtension($container->get('twig.http_router_extension'));
+        }
+    ],
 ];
