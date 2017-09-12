c40389cbafb8949a06ff3b4dc19a94abd859a8a2
blumfontein <i.makashev@naimi.kz>
2017-09-12 00:40:23 +0000

    Timestamp to assets

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -19,6 +19,30 @@ class HomeController extends TemplateController
             ->orderByToolbar()
             ->find();
 
-        $this->getView()->addVar('toolbars', $toolbars);
+        $this->getView()->addVars([
+            'toolbars' => $toolbars,
+            'static_version' => $this->getStaticVersion()
+        ]);
+    }
+
+    private function getStaticVersion()
+    {
+        if (ENV === 'dev') {
+            $static_version = null;
+        } else {
+            $cache = $this->getContainer()->get('cache.file')->getItem('static.version');
+
+            $static_version = $cache->get();
+
+            if ($cache->isMiss()) {
+                $cache->lock();
+
+                $static_version = time();
+
+                $cache->set($static_version, 30 * 86400);
+            }
+        }
+
+        return $static_version;
     }
 }
